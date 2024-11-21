5c9e1e8f9e2f8e8c861a54faa2e997b5d6dead11
RyandersonSilva <ryandersonsilva77@gmail.com>
2024-11-21 12:52:54 -0300

    Teste

--- main.c
+++ main.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+void imprime_histograma(int *vendas[], int num_categorias) {
+    for (int i = 0; i < num_categorias; i++) {
+        printf("%d ", *(vendas[i]));
+        for (int j = 0; j < *(vendas[i]); j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
-    
+int main() {
+    int num_categorias;
+
+    printf("Digite o número de categorias: ");
+    scanf("%d", &num_categorias);
+
+    int **vendas = (int**)malloc(num_categorias * sizeof(int*));
+
+    for (int i = 0; i < num_categorias; i++) {
+        vendas[i] = (int*)malloc(sizeof(int));
+        printf("Digite a quantidade de vendas da categoria %d: ", i+1);
+        scanf("%d", vendas[i]);
+    }
+
+    imprime_histograma(vendas, num_categorias);
+
+    for (int i = 0; i < num_categorias; i++) {
+        free(vendas[i]);
+    }
+    free(vendas);
+
     return 0;
 }
